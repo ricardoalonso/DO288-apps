ebff341ae063868d665ab154be317db4f25c12e0
Ricardo Alonso <ricardo.alonso@r2.ricardoalonso.local>
2020-04-29 18:29:10 +0100

    post function

--- nodejs-helloworld/app.js
+++ nodejs-helloworld/app.js
@@ -5,6 +5,11 @@ app.get('/', function (req, res) {
   res.send('Hello World!\n');
 });
 
+app.get('/post-dnsmasq', function (req, res) {
+  console.log('Body:', req.body);
+  res.sendStatus(200);
+});
+
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
 });
