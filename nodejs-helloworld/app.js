79b721d1debd74cd6a7bbf26bb0fe8524086793c
Ricardo Alonso <ricardo.alonso@r2.ricardoalonso.local>
2020-04-29 18:56:13 +0100

    fixed app.js

--- nodejs-helloworld/app.js
+++ nodejs-helloworld/app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const bodyParser = require('body-parser);
+const bodyParser = require('body-parser');
 app = express();
 app.use(bodyParser.urlencoded({ extended: true });
 app.use(bodyParser.json());
