4e4be802e5ea4e54c6c2cf6ceb78eaf6deffbba4
Student User <student@workstation.lab.example.com>
2020-11-11 09:22:12 +0000

    fixing date

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,3 +1,3 @@
 <?php
-print "Today is " . date() . ".Hello, World! php version is " . PHP_VERSION . "\nWelcome to the DO288 Class!.";
+print "Today is " . date('l jS \of F Y h:i:s A') . ".Hello, World! php version is " . PHP_VERSION . "\nWelcome to the DO288 Class!.";
 ?>
