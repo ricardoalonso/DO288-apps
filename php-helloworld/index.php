aab2e94e638bccd531764887f292d19a1e6234a5
Student User <student@workstation.lab.example.com>
2020-11-25 08:53:41 -0500

    version 3.1

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,4 +1,4 @@
 <?php
 print "Today is " . date('l jS \of F Y h:i:s A') . ".Hello, World! php version is " . PHP_VERSION . "\nWelcome to the DO288 Class!.";
-print "Version 3.0";
+print "Version 3.1";
 ?>
